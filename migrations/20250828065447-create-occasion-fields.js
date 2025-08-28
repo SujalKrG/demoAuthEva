c74296b56f8a69e5a271e3483039eadc87457b57
sujalkrg <sujalkrghosh@gmail.com>
2025-08-28 15:44:29 +0530

    Occasions migration file and model created

---
+++ migrations/20250828065447-create-occasion-fields.js
@@ -0,0 +1,65 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("occasion_fields", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      occasion_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      field_key: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      required: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      options: {
+        type: Sequelize.JSON,
+        allowNull: true,
+      },
+      order_no: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+
+    await queryInterface.addIndex("occasion_fields", [
+      "occasion_id",
+      "order_no",
+    ]);
+    await queryInterface.addIndex("occasion_fields", ["field_key"]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("occasion_fields");
+  },
+};
