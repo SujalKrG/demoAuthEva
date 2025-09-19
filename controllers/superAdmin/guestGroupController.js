bd59d5f43a51593e4480edc2cc4456de9a343220
Aloka8018 <kumaraloka7205@gmail.com>
2025-09-19 11:14:42 +0530

    redis error solved

--- controllers/superAdmin/guestGroupController.js
+++ controllers/superAdmin/guestGroupController.js
@@ -3,14 +3,25 @@ import db from "../../models/index.js";
 
 export const createGuestGroups = async (req, res) => {
   try {
-    const {user_id, name } = req.body;
+    const { user_id, name } = req.body;
     if (!name) {
       return res.status(400).json({
         success: false,
         message: "Name is required",
       });
-
-
     }
-  } catch (error) {}
+    
+
+  } catch (error) {
+    const handled = handleSequelizeError(error, res);
+    if (handled) return handled;
+
+    return res.status(500).json({
+      message: "Unexpected server error",
+      error:
+        process.env.NODE_ENV === "production"
+          ? "Internal Server Error"
+          : error.message,
+    });
+  }
 };
