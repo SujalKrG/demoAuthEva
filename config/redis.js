4c7da5e84b09cadd860e87a816b865453c447021
Sujal Kumar Ghosh <131712651+SujalKrG@users.noreply.github.com>
2025-09-19 12:13:22 +0530

    Update redis.js

--- config/redis.js
+++ config/redis.js
@@ -1,8 +1,8 @@
 export const redisConfig = {
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  // username: process.env.REDIS_USERNAME,
-  // password: process.env.REDIS_PASSWORD,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
 };
 
 
