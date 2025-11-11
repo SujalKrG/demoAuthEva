1ad0d3cdac4abb249c21d881f640ab5ec2841100
Evatril <evatrilofficial@gmail.com>
2025-11-11 16:28:42 +0530

    Rename 'order_no' to 'schedule_no' in invitation_schedules

--- migrations/20251106044651-rename-order_no-to-schedule_no-in-invitation_schedule-table.js
+++ migrations/20251106044651-rename-order_no-to-schedule_no-in-invitation_schedule-table.js
@@ -5,16 +5,16 @@ export default {
   async up(queryInterface, Sequelize) {
     queryInterface.renameColumn(
       "invitation_schedules",
-      "schedule_no",
       "order_no",
+      "schedule_no",
     );
   },
 
   async down(queryInterface, Sequelize) {
     queryInterface.renameColumn(
       "invitation_schedules",
-      "order_no",
-      "schedule_no",
+       "schedule_no",
+       "order_no",
     );
   },
 };
